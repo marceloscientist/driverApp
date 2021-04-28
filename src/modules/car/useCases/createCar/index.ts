19c9b6c2319833b20ca11b853704fe08323eb709
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-04-28 10:21:51 -0300

    Swagger inserted

--- src/modules/car/useCases/createCar/index.ts
+++ src/modules/car/useCases/createCar/index.ts
@@ -2,6 +2,7 @@ import { CarsRepository } from "../../repositories/Implementations/CarsRepositor
 
 import { CreateCarUseCase } from "../../useCases/createCar/CreateCarUseCase";
 import { CreateCarController } from "../../useCases/createCar/CreateCarController";
+
 const carsRepository = CarsRepository.getInstance();
 const createCarUseCase = new CreateCarUseCase(carsRepository);
 const createCarController = new CreateCarController(createCarUseCase);
